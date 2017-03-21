1c6c95953929e26f5ce18a00fe8b153543bf6cc1
Pilmo Kang <ivorycirrus@gmail.com>
2017-03-21 21:58:32 +0900

    write CRUD commands for sequelize

--- MysqlRestORM/REST.js
+++ MysqlRestORM/REST.js
@@ -8,6 +8,8 @@ function REST_ROUTER(router,objSequlize) {
 
 REST_ROUTER.prototype.handleRoutes = function(router,objSequlize) {
     var self = this;
+
+    // Define Model
     var Todo = objSequlize.define('todo', {
         todo_id: {
             type: Sequelize.INTEGER,
@@ -42,105 +44,89 @@ REST_ROUTER.prototype.handleRoutes = function(router,objSequlize) {
         tableName: 'todo'
     });
 
+    // [GET] for api test
     router.get("/",function(req,res){
+        console.log("[Success] GET /");
         res.json({"Message" : "Hello World !"});
     });
 
+    // [GET] retrieve all to-do list
     router.get("/todo",function(req,res){
         Todo.findAll({
-                attributes : ['todo_id', 'todo', 'status', 'due']
-            })
-            .then(function(todos){
-                res.json({"Error" : false, "Message" : "Success", "Todo" : todos});
-            })
-            .catch(function(err){
-                console.log("[Error] GET /todo | "+err);
-                res.json({"Error" : true, "Message" : "Error executing query!!"});
-            })
-            .done();
+            attributes : ['todo_id', 'todo', 'status', 'due']
+        })
+        .then(function(todos){
+            console.log("[Success] GET /todo/");
+            res.json({"Error" : false, "Message" : "Success", "Todo" : todos});
+        })
+        .catch(function(err){
+            console.log("[Error] GET /todo | "+err);
+            res.json({"Error" : true, "Message" : "Error executing query!!"});
+        })
+        .done();
     });
 
+    // [GET] retrieve specfic to-do
     router.get("/todo/:todo_id",function(req,res){
         Todo.findAll({
-                where : {todo_id : req.params.todo_id}
-            })
-            .then(function(todos){
-                res.json({"Error" : false, "Message" : "Success", "Todo" : todos});
-            })
-            .catch(function(err){
-                console.log("[Error] GET /todo | "+err);
-                res.json({"Error" : true, "Message" : "Error executing query!!"});
-            })
-            .done();
+            attributes : ['todo_id', 'todo', 'status', 'due'],
+            where : {todo_id : req.params.todo_id}
+        })
+        .then(function(todos){
+            console.log("[Success] GET /todo/"+req.params.todo_id);
+            res.json({"Error" : false, "Message" : "Success", "Todo" : todos});
+        })
+        .catch(function(err){
+            console.log("[Error] GET /todo | "+err);
+            res.json({"Error" : true, "Message" : "Error executing query!!"});
+        })
+        .done();
     });
 
+    // [POST] add a to-do
     router.post("/todo",function(req,res){
-        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
-        var table = ["todo","todo","status",req.body.todo,req.body.status];
-        query = mysql.format(query,table); 
-
-        connectionPool.getConnection(function(connErr,connection){
-            if(connErr) {
-                res.json({"Error" : true, "Message" : "Error MySQL connection"});
-                if(connection) connection.release();
-                return;
-            }
-
-            connection.query(query,function(err,rows){
-                if(err) {
-                    res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-                } else {
-                    res.json({"Error" : false, "Message" : "Todo Added !"});
-                }
-                if(connection) connection.release();
-            });
-        });
+        Todo.create({ 
+            todo : req.body.todo,
+            status : req.body.status
+        })
+        .then(function(task) {
+            console.log("[Success] POST /todo");
+            res.json({"Error" : false, "Message" : "Todo Added !"});
+        })
+        .catch(function() {
+            console.log("[Error] POST /todo | "+err);
+            res.json({"Error" : true, "Message" : "Error executing query!!"});
+        })
+        .done();
     });
 
+    // [PUT] update a to-do
     router.put("/todo/:todo_id",function(req,res){
-        var query = "UPDATE ?? SET ?? = ? , ?? = ? WHERE ?? = ?";
-        var table = ["todo","todo",req.body.todo,"status",req.body.status,"todo_id",req.params.todo_id];
-        query = mysql.format(query,table);
-
-        connectionPool.getConnection(function(connErr,connection){
-            if(connErr) {
-                res.json({"Error" : true, "Message" : "Error MySQL connection"});
-                if(connection) connection.release();
-                return;
-            }
-
-            connection.query(query,function(err,rows){
-                if(err) {
-                    res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-                } else {
-                    res.json({"Error" : false, "Message" : "Updated the status for "+req.params.todo_id});
-                }
-                if(connection) connection.release();
-            });
-        });
+        Todo.update(
+            { todo : req.body.todo, status : req.body.status }, // data
+            { where : {todo_id : req.params.todo_id}, returning: true } // condition
+        ).then(function(result){
+            console.log("[Success] PUT /todo/"+req.params.todo_id+" | rows : "+result);
+            res.json({"Error" : false, "Message" : "Updated the status for "+req.params.todo_id});
+        }).catch(function() {
+            console.log("[Error] PUT /todo/"+req.params.todo_id+" | "+err);
+            res.json({"Error" : true, "Message" : "Error executing query!!"});
+        })
+        .done();
     });
 
+    // [DELETE] remove a to-do
     router.delete("/todo/:todo_id",function(req,res){
-        var query = "DELETE from ?? WHERE ??=?";
-        var table = ["todo","todo_id",req.params.todo_id];
-        query = mysql.format(query,table);
-
-        connectionPool.getConnection(function(connErr,connection){
-            if(connErr) {
-                res.json({"Error" : true, "Message" : "Error MySQL connection"});
-                if(connection) connection.release();
-                return;
-            }
-
-            connection.query(query,function(err,rows){
-                if(err) {
-                    res.json({"Error" : true, "Message" : "Error executing MySQL query"});
-                } else {
-                    res.json({"Error" : false, "Message" : "Deleted todo "+req.params.todo_id});
-                }
-                if(connection) connection.release();
-            });
-        });
+        Todo.destroy({
+            where: {todo_id : req.params.todo_id}
+        }).then(function(result) {
+            console.log("[Success] DELETE /todo/"+req.params.todo_id+" | rows : "+result);
+            res.json({"Error" : false, "Message" : "Deleted todo "+req.params.todo_id});
+        }).catch(function(err) {
+            console.log("[Error] DELETE /todo/"+req.params.todo_id+" | "+err);
+            res.json({"Error" : true, "Message" : "Error executing query!!"});
+        })
+        .done();
     });
 }
 
